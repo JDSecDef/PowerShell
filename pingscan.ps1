9ad453c68d33317792a58175214d34ee9903d329
JDSecDef <52600556+JDSecDef@users.noreply.github.com>
2019-10-15 13:53:00 +1100

    Add files via upload
    PowerShell script that performs a ping scan of an IP range to detect if hosts are up or not.

---
+++ pingscan.ps1
@@ -0,0 +1,21 @@
+﻿# This script pings an IP Range to check if hosts are up 
+
+$subnet = Read-Host -prompt "`nEnter IP block"
+[int]$ipstart = Read-Host -Prompt "Enter the start IP address"
+[int]$ipend = Read-Host -Prompt "Enter the End IP address"
+[int]$delay = 500
+
+Write-Host ("`nPinging IP Range $subnet" + (".") + "$ipstart" + ("-") + "$ipend") -ForegroundColor Yellow
+
+Measure-Command {
+while ($ipstart -le $ipend) {
+$ip = $subnet + (".") + $ipstart
+$test = Test-Connection -ComputerName $ip -count 1 -Quiet -ErrorAction SilentlyContinue 
+    if ($test -eq $true) {
+    write-host "$ip Host is UP" -ForegroundColor Green }
+    else {
+    write-host "$ip Host did not respond" -ForegroundColor Red
+    }
+$ipstart++
+} 
+} | select -Property TotalMinutes, TotalSeconds

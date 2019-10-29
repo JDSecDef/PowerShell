10edea5952e4470763bb65b27e99d1b195632fdc
JDSecDef <james.duncan@outlook.com>
2019-10-29 23:03:13 +1100

    Updates

--- pingscan.ps1
+++ pingscan.ps1
@@ -6,6 +6,9 @@
 .EXAMPLE
     PS C:\> pingscan.ps1 10.0.0.[1-255]
     This will ping this host range. Explanation of what the example does
+.EXAMPLE
+    PS C:\> pingscan.ps1 10.0.0.1
+    This will ping a single IP Address
 .INPUTS
     Inputs (if any)
 .OUTPUTS
@@ -16,32 +19,54 @@
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$true)]
-    [array]$IPAddress
+    [Parameter(Mandatory=$true,
+    HelpMessage="Example - 10.0.0.[1-255] or 10.0.0.1")]
+    [String[]]
+    $IPAddress
 )
 
 $InformationPreference = "Continue"
 $Seperator = ".""[""-"
 
+function PingIPRange {
+    Measure-Command {
+    Write-Information ("`nPinging IP Range $FormatIPAddress" + "$IPRangeStart" + ("-") + "$IPRangeEnd")
+    while ($IPRangeStart -le $IPRangeEnd) {
+        $IP = $FormatIPAddress + $IPRangeStart
+        $PingIP = Test-Connection -ComputerName $IP -count 1 -Quiet -ErrorAction SilentlyContinue 
+        if ($PingIP -eq $true) {
+            Write-Information "$IP Host is UP" }
+        else {
+            Write-Information "$IP Host did not respond"
+        }
+        $IPRangeStart++
+    } 
+    } | Select-Object -Property TotalMinutes, TotalSeconds
+}
+
+function PingIP {
+    Measure-Command {
+    $IP = $FormatIPAddress
+    Write-Information ("`nPinging IP Address $FormatIPAddress")
+    $PingIP = Test-Connection -ComputerName $IP -count 1 -Quiet -ErrorAction SilentlyContinue 
+    if ($PingIP -eq $true) {
+        Write-Information "$IP Host is UP" }
+    else {
+        Write-Information "$IP Host did not respond"
+    }
+    } | Select-Object -Property @{label='Seconds';expression={$_.TotalSeconds}}
+}
+
 $IPAddress | ForEach-Object {
     $IPAddress = $_.split($Seperator)
-    }
+}
 
-$FormatIPAddress = $IPAddress[0,1,2,3] -join '.'
-[int]$IPRangeStart = $IPAddress[4]
-[int]$IPRangeEnd = $IPAddress[5] -replace '[[\]"]' 
-
-Write-Information ("`nPinging IP Range $FormatIPAddress" + "$IPRangeStart" + ("-") + "$IPRangeEnd")
-
-Measure-Command {
-while ($IPRangeStart -le $IPRangeEnd) {
-$IP = $FormatIPAddress + $IPRangeStart
-$PingIP = Test-Connection -ComputerName $IP -count 1 -Quiet -ErrorAction SilentlyContinue 
-    if ($PingIP -eq $true) {
-    Write-Information "$IP Host is UP" }
-    else {
-    Write-Information "$IP Host did not respond"
-    }
-$IPRangeStart++
-} 
-} | Select-Object -Property TotalMinutes, TotalSeconds
+if ($IPAddress[5] -match "\]") { 
+    $FormatIPAddress = $IPAddress[0,1,2,3] -join '.'
+    [int]$IPRangeStart = $IPAddress[4]
+    [int]$IPRangeEnd = $IPAddress[5] -replace '[[\]"]' 
+    PingIPRange
+} if ($IPAddress[3] -match "^\d+$")  {
+    $FormatIPAddress = $IPAddress[0,1,2,3] -join '.' 
+    PingIP
+}

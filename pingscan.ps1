93b6bd61f2b1cab5b16bdbadc27cef2213eaf71e
JDSecDef <james.duncan@outlook.com>
2019-10-29 16:20:55 +1100

    Update1

--- pingscan.ps1
+++ pingscan.ps1
@@ -1,7 +1,28 @@
-# This script pings an IP Range to check if hosts are up 
+<#
+.SYNOPSIS
+    Ping hosts to check for response.
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> pingscan.ps1 10.0.0.[1-255]
+    This will ping this host range. Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
 
-$IPAddress = Read-Host "`nEnter IP Address Range to scan"
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [array]$IPAddress
+)
+
+$InformationPreference = "Continue"
 $Seperator = ".""[""-"
+
 $IPAddress | ForEach-Object {
     $IPAddress = $_.split($Seperator)
     }
@@ -10,16 +31,16 @@ $FormatIPAddress = $IPAddress[0,1,2,3] -join '.'
 [int]$IPRangeStart = $IPAddress[4]
 [int]$IPRangeEnd = $IPAddress[5] -replace '[[\]"]' 
 
-Write-Host ("`nPinging IP Range $FormatIPAddress" + "$IPRangeStart" + ("-") + "$IPRangeEnd") -ForegroundColor Yellow
+Write-Information ("`nPinging IP Range $FormatIPAddress" + "$IPRangeStart" + ("-") + "$IPRangeEnd")
 
 Measure-Command {
 while ($IPRangeStart -le $IPRangeEnd) {
 $IP = $FormatIPAddress + $IPRangeStart
 $PingIP = Test-Connection -ComputerName $IP -count 1 -Quiet -ErrorAction SilentlyContinue 
     if ($PingIP -eq $true) {
-    write-host "$IP Host is UP" -ForegroundColor Green }
+    Write-Information "$IP Host is UP" }
     else {
-    write-host "$IP Host did not respond" -ForegroundColor Red
+    Write-Information "$IP Host did not respond"
     }
 $IPRangeStart++
 } 

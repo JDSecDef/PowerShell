317af529ff83cb2c89bfdafee6857507aefcbb77
JDSecDef <james.duncan7@defence.gov.au>
2022-12-27 22:51:06 +1100

    Updates to Search-Filename.psm1 script

--- Misc/Search-Filename.psm1
+++ Misc/Search-Filename.psm1
@@ -8,29 +8,34 @@ function Search-Filename {
     and stringing together multiple search words in a single search. The command must be run as a user
     who has access to the files being searched. 
     
-    .PARAMETER Path
+    .PARAMETER SearchPath
     Provide a folder path or fileshare to search. 
+
+    .PARAMETER SaveOutputPath
+    Provide a folder path and filename to save the output to. 
+
     .PARAMETER SearchWord
     Provide one or more search words to use as the search criteria. If no search words are provided than
     the default search words will be used - "*password*","*protected*","*.mp4","*.exe","*dvd*","*secret*",
     "*.bat","*.kbdx","*.ps1".
+
     .EXAMPLE
     PS C:\> Search-Filename -Path C:\ -Keywords "*.txt","*.csv","*sensitive*"'
     This will recursively search the C:\ for filenames containing the provided keywords.     
     
     .NOTES
-    Author      : JDSecDef
-    Version     : 1.0.0
-    Last Updated: 18 November 2019
+    Version     : 1.0.1
+    Last Updated: 15 September 2021
     #>
     
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipeline = $true,
-                   Mandatory = $true,
-                   HelpMessage = 'Example - Search-Filename -Path C:\ -Keywords "*.txt","*.csv","*sensitive*"')]
-        [Alias('Path')]
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true, HelpMessage = 'Example - Search-Filename -FilePath C:\ -SavePath C:\Temp\output.csv -Keywords "*.txt","*.csv","*sensitive*"')]
+        [Alias('SearchPath')]
         [String]$FilePath,
+        [Alias('SaveOutputPath')]
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [String]$SavePath,
         [Alias('SearchWord')]
         [String[]]$SearchWords = ("*password*","*protected*","*.mp4","*.exe","*dvd*","*secret*","*.bat","*.kbdx","*.ps1")
     )
@@ -59,6 +64,7 @@ function Search-Filename {
         } # Catch
         Finally {
             Write-Output $SearchResults
+            $SearchResults | Out-File $SavePath -Append
         } # Finally 
     } # Process
     End {}
